310b7c6439f1f1831bb16cb72c1336bd100aa560
Shing Hung <yhung022@ucr.edu>
2024-01-24 15:40:34 -0800

    fix color

--- src/components/static/Header.jsx
+++ src/components/static/Header.jsx
@@ -1,11 +1,11 @@
 const Header = ({ text, color }) => {
   return (
     <div
-      className={`w-8/12 ml-[10%] text-[80px] leading-none text-hackathon-gray-100 font-bold font-poppins italic relative border-b-[6px] border-hackathon-${color} pb-2`}
+      className={`w-8/12 ml-[10%] text-[80px] leading-none text-hackathon-gray-100 font-bold font-poppins italic relative border-b-[6px] border-citrus-${color} pb-2`}
     >
       {text}
       <p
-        className={`!text-[40px] pb-2 text-black font-extrabold absolute bottom-0 border-b-[10px] pr-[7%] border-hackathon-${color}`}
+        className={`!text-[40px] pb-2 text-black font-extrabold absolute bottom-0 border-b-[10px] pr-[7%] border-citrus-${color}`}
       >
         {text}
       </p>
