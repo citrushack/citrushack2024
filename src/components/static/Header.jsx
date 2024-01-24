39e00855d183eca96a618c2e613080a2961c1d76
Shing Hung <yhung022@ucr.edu>
2024-01-24 15:04:23 -0800

    Implement Header

---
+++ src/components/static/Header.jsx
@@ -0,0 +1,16 @@
+const Header = ({ text, color }) => {
+  return (
+    <div
+      className={`w-8/12 ml-[10%] text-[80px] leading-none text-hackathon-gray-100 font-bold font-poppins italic relative border-b-[6px] border-hackathon-${color} pb-2`}
+    >
+      {text}
+      <p
+        className={`!text-[40px] pb-2 text-black font-extrabold absolute bottom-0 border-b-[10px] pr-[7%] border-hackathon-${color}`}
+      >
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default Header;
