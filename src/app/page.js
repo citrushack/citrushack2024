979289bc7a848e26feaf6a50116aa642ab2d55a1
Shing Hung <yhung022@ucr.edu>
2024-01-24 16:34:11 -0800

    fix font

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Release from "@/components/static/Release";
 import RELEASES from "@/data/Releases";
 import Live from "@/components/static/live/Live";
 import Postlive from "@/components/static/postlive/Postlive";
-import Header from "@/components/static/Header";
+
 const Home = () => {
   return (
     <div className="w-full">
@@ -12,7 +12,6 @@ const Home = () => {
       <Release release={RELEASES.STATIC.POSTLIVE}>
         <Postlive />
       </Release>
-      <Header color="green-200" text="TRACKS" />
     </div>
   );
 };
