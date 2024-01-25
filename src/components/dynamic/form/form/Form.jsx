af82ee8830c9ad120ec4eebda2d744c98d7aea78
menthy-wu <wumengxuan0728@gmail.com>
2024-01-25 11:40:34 -0800

    add logo

--- src/components/dynamic/form/form/Form.jsx
+++ src/components/dynamic/form/form/Form.jsx
@@ -6,6 +6,8 @@ import Questions from "./Questions";
 import Confirmation from "./Confirmation";
 import Button from "../../Button";
 import { signOut } from "next-auth/react";
+import Image from "next/image";
+import logo from "@/public/LOGO.svg";
 
 const Form = ({
   object,
@@ -31,8 +33,9 @@ const Form = ({
           color="green"
         />
       </div>
-      <div className="w-10/12 md:w-1/2 xl:w-1/3 my-5 ">
-        <p className="text-xl bg-citrus-green-100 font-semibold px-4 py-2 rounded-t-xl m-0">
+      <div className="w-10/12 md:w-1/2 xl:w-1/3 my-5 flex flex-col items-center justify-center">
+        <Image src={logo} className="w-1/2" />
+        <p className="mt-6 w-full text-xl bg-citrus-green-100 font-semibold px-4 py-2 rounded-t-xl m-0">
           {header}
         </p>
         <div className="rounded-b-xl bg-citrus-beige-100 p-3">
