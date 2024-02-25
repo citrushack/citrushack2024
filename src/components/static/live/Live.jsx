8facc0da71b3ef63ebdee90094eed2976fb1c080
menthy-wu <wumengxuan0728@gmail.com>
2024-02-24 22:22:53 -0800

    about

--- src/components/static/live/Live.jsx
+++ src/components/static/live/Live.jsx
@@ -1,9 +1,11 @@
+import About from "./about/About";
 import Landing from "./landing/Landing";
 
 const Live = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <Landing />
+      <About />
     </div>
   );
 };
